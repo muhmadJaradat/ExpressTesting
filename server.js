6f56bc958901a33e42e2ca40f38d7d291ff125f2
muhmadjaradat <muhmadjradat@gmail.com>
2021-05-04 15:51:21 +0300

    adding lab07

--- server.js
+++ server.js
@@ -1,8 +1,28 @@
 const express = require('express')
 const app = express()
- 
+require('dotenv').config()
+let PORT=process.env.PORT
+const data=require('./data/weather.json')
+console.log(data.data);
+const cors =require('cors');
+app.use(cors());
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
- 
-app.listen(3000)
+
+
+app.get('/weather', function (req, res) {
+  let filteredData=data.data.map(data=>{
+    return( new Forcast(data))
+  })
+  res.send(filteredData)
+})
+app.listen(PORT,()=> console.log(`listing to the ${PORT}`))
+
+class Forcast{
+  constructor(data){
+    this.description=data.weather.description;
+    this.date=data.valid_date;
+  }
+}
